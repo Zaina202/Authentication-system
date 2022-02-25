12c1c1b767b67d3ba1dd179fb5a66bd05fe84d77
Tasnim <s12028850@stu.najah.edu>
2022-02-25 16:07:01 +0200

    Connect NodeJS and MySQL Database

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express=require('express');
-const httpServer=express();
+const mysql=require('mysql');
 
+const app=express();
 
+var connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"signup_db",
+    port:"3306"
 
-
-
-
-
-
-
-httpServer.listen(3200,()=>{
-    console.log("server is running on 3200")
 })
+connection.connect((err)=>{
+    if(err){
+        throw err
+    }else{
+        console.log("connected")
+    }
+})
+connection.query('CREATE TABLE users(First_NAME VARCHAR(255) NOT NULL,Last_NAME VARCHAR(255) NOT NULL, Email VARCHAR(255) NOT NULL, Password VARCHAR(255) NOT NULL,Confirm_Password VARCHAR(255) NOT NULL)',(err,rows)=>{
+    if(err){
+               throw err;
+            }else{
+                console.log('data set bois');
+                 console.log(rows);
+            }
+})
+
+const port=process.env.PORT||4000;
+app.listen(port);
+
+console.log("APP is listening on port"+port)
