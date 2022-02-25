b2282930cc4923885dc7a7978449f15a2464b951
deemashahwan <92646601+deemashahwan@users.noreply.github.com>
2022-02-25 12:52:53 +0300

    nothing

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const httpServer=express();
 
-httpServer.get('/',(req,res,next)=>{
-    res.send('hi');
-})
+
+
+
+
+
+
+
 
 httpServer.listen(3200,()=>{
     console.log("server is running on 3200")
